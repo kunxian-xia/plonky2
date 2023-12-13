eb3d67556e68db2881953597f387ed0c65bd5f35
kunxian xia <xiakunxian130@gmail.com>
2023-12-14 01:00:18 +0800

    add fibonacci stark bench

--- bench.sh
+++ bench.sh
@@ -1,3 +1,5 @@
+# fibonacci stark over 600 columns and 1 << 21 rows
+RUST_LOG=debug cargo test --release --package starky --lib fibonacci_stark::tests::test_fibonacci_stark_prover -- --nocapture
 # fft for polynomial of degrees from 19 to 23
 # lde onto coset for polynomial of degrees from 20 to 23 and rate_bits from 1 to 3.
 cargo bench --package plonky2 --bench ffts
