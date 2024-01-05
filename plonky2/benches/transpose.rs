3f08897e0dbfa055303db0617ab6961969eee21b
kunxian xia <xiakunxian130@gmail.com>
2024-01-05 21:56:27 +0800

    expand rows to a larger interval for transpose benches

--- plonky2/benches/transpose.rs
+++ plonky2/benches/transpose.rs
@@ -10,17 +10,20 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     // In practice, for the matrices we care about, each row is associated with a polynomial of
     // degree 2^13, and has been low-degree extended to a length of 2^16.
-    const WIDTH: usize = 1 << 16;
-
-    let mut group = c.benchmark_group("transpose");
+    // const WIDTH: usize = 1 << 16;
 
     // We have matrices with various numbers of polynomials. For example, the witness matrix
     // involves 100+ polynomials.
-    for height in [5, 50, 100, 150] {
-        group.bench_with_input(BenchmarkId::from_parameter(height), &height, |b, _| {
-            let matrix = (0..height).map(|_| F::rand_vec(WIDTH)).collect::<Vec<_>>();
-            b.iter(|| transpose(&matrix));
-        });
+    for lg_width in [16, 17, 18, 19, 20, 21, 22, 23] {
+        let mut group = c.benchmark_group(&format!("transpose<rows=2^{}>", lg_width));
+        for height in [5, 50, 100, 150, 200, 400, 600, 700] {
+            group.bench_with_input(BenchmarkId::from_parameter(height), &height, |b, _| {
+                let matrix = (0..height)
+                    .map(|_| F::rand_vec(1 << lg_width))
+                    .collect::<Vec<_>>();
+                b.iter(|| transpose(&matrix));
+            });
+        }
     }
 }
 
