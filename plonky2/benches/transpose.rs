29ae11bf8c8d330e57866ce7b643772f53e9759b
kunxian xia <xiakunxian130@gmail.com>
2024-01-24 10:08:29 +0800

    refine transpose bench

--- plonky2/benches/transpose.rs
+++ plonky2/benches/transpose.rs
@@ -1,6 +1,6 @@
 mod allocator;
 
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, BatchSize};
 use plonky2::field::goldilocks_field::GoldilocksField;
 use plonky2::field::types::Sample;
 use plonky2::util::transpose;
@@ -14,14 +14,18 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     // We have matrices with various numbers of polynomials. For example, the witness matrix
     // involves 100+ polynomials.
-    for lg_width in [16, 17, 18, 19, 20, 21, 22, 23] {
+    for lg_width in [19, 20, 21, 22, 23] {
         let mut group = c.benchmark_group(&format!("transpose<rows=2^{}>", lg_width));
+        let rand_vec = F::rand_vec(1 << lg_width);
         for height in [5, 50, 100, 150, 200, 400, 600, 700] {
             group.bench_with_input(BenchmarkId::from_parameter(height), &height, |b, _| {
-                let matrix = (0..height)
-                    .map(|_| F::rand_vec(1 << lg_width))
-                    .collect::<Vec<_>>();
-                b.iter(|| transpose(&matrix));
+                b.iter_batched(
+                    || {
+                        (0..height).map(|_| rand_vec.clone()).collect::<Vec<_>>()
+                    },
+                    |matrix| transpose(&matrix),
+                    BatchSize::SmallInput,
+                );
             });
         }
     }
